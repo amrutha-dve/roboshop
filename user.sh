18c3508483656e71a8bf904d3b102db161c66680
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-14 04:41:33 +0530

    user script

--- user.sh
+++ user.sh
@@ -3,7 +3,6 @@ cp user.service /etc/systemd/system/user.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app
