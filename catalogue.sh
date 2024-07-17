c6eae216d343b4601951719a07e1b6bb16b19ee2
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-17 07:54:53 +0530

    redirecting

--- catalogue.sh
+++ catalogue.sh
@@ -2,8 +2,8 @@ source common.sh
 component=catalogue
 NODEJS
 
-PRINT install mongo client
-dnf install mongodb-mongosh -y &>>$LOG_FILE
+echo install mongo client
+dnf install mongodb-mongosh -y >$LOG_FILE
 
-PRINT load master data
-mongosh --host mongo.dev.devopsb72.online </app/db/master-data.js &>>$LOG_FILE
+echo load master data
+mongosh --host mongo.dev.devopsb72.online </app/db/master-data.js >$LOG_FILE
--- common.sh
+++ common.sh
@@ -1,47 +1,47 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
-PRINT() {
-  echo &>>LOG_FILE
-  echo &>>LOG_FILE
-  echo "####################  $*  ###############" &>>LOG_FILE
-  echo $*
-}
+#PRINT() {
+# echo &>>LOG_FILE
+  #echo &>>LOG_FILE
+  #echo "####################  $*  ###############" &>>LOG_FILE
+  #echo $*
+$}
 NODEJS() {
- PRINT DisableNodeJS Default Version
- dnf module disable nodejs -y &>>$LOG_FILE
- PRINT enable NodeJS 20 Module
- dnf module enable nodejs:20 -y &>>$LOG_FILE
+ echo DisableNodeJS Default Version
+ dnf module disable nodejs -y >$LOG_FILE
+ echo enable NodeJS 20 Module
+ dnf module enable nodejs:20 -y >$LOG_FILE
 
- PRINT Install NodeJS
- dnf install nodejs -y &>>$LOG_FILE
+ echo Install NodeJS
+ dnf install nodejs -y >$LOG_FILE
 
- PRINT Copy Service File
- cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
+ echo Copy Service File
+ cp ${component}.service /etc/systemd/system/${component}.service >$LOG_FILE
 
- PRINT copy MongoDB repo File
- cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+ echo copy MongoDB repo File
+ cp mongo.repo /etc/yum.repos.d/mongo.repo >$LOG_FILE
 
- PRINT adding Application User
- useradd roboshop &>>/$LOG_FILE
+ echo adding Application User
+ useradd roboshop >$LOG_FILE
 
- PRINT Cleaning Old Content
- rm -rf /app &>>$LOG_FILE
+ echo Cleaning Old Content
+ rm -rf /app >$LOG_FILE
 
- PRINT creating App Directory
- mkdir /app &>>$LOG_FILE
+ echo creating App Directory
+ mkdir /app >$LOG_FILE
 
- PRINT downloading the app info
- curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/ca${component}-v3.zip &>>$LOG_FILE
+echo downloading the app info
+ curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/ca${component}-v3.zip >$LOG_FILE
  cd /app
 
- PRINT Extract the app content
- unzip /tmp/${component}.zip &>>$LOG_FILE
+echo Extract the app content
+ unzip /tmp/${component}.zip >$LOG_FILE
 
- PRINT download NodeJS depedencies
- npm install &>>$LOG_FILE
+echo download NodeJS depedencies
+ npm install >$LOG_FILE
 
- PRINT Start Service
- systemctl daemon-reload &>>$LOG_FILE
- systemctl enable ${component} &>>$LOG_FILE
- systemctl restart ${component} &>>$LOG_FILE
+ echo Start Service
+ systemctl daemon-reload >$LOG_FILE
+ systemctl enable ${component} >$LOG_FILE
+ systemctl restart ${component} >$LOG_FILE
 }
