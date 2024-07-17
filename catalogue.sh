b5f3012aa97a8dbb56ad865d73083608ed1547d4
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-17 06:39:27 +0530

    /app update

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,9 @@
-## updating service file
-cp catalogue.service /etc/systemd/system/catalogue.service
-## updating repo file
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
+source common.sh
+component=catalogue
+NODEJS
 
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
-dnf install mongodb-mongosh -y
-mongosh --host mongo.dev.devopsb72.online </app/db/master-data.js
+echo install mongo client
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+
+echo load master data
+mongosh --host mongo.dev.devopsb72.online </app/db/master-data.js &>>$LOG_FILE
---
+++ common.sh
@@ -0,0 +1,49 @@
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+PRINT() {
+  echo &>>LOG_FILE
+  echo &>>LOG_FILE
+  echo "####################  $*  ###############" &>>LOG_FILE
+  echo $*
+}
+NODEJS() {
+ PRINT DisableNodeJS Default Version
+ dnf module disable nodejs -y &>>$LOG_FILE
+ PRINT enable NodeJS 20 Module
+ dnf module enable nodejs:20 -y &>>$LOG_FILE
+
+ PRINT Install NodeJS
+ dnf install nodejs -y &>>/tmp/roboshop.log
+
+ PRINT Copy Service File
+ cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
+
+ PRINT copy MongoDB repo File
+ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+
+ PRINT adding Application User
+ useradd roboshop &>>/$LOG_FILE
+
+ PRINT Cleaning Old Content
+ rm -rf /app &>>$LOG_FILE
+
+ PRINT creating App Directory
+ mkdir /app &>>$LOG_FILE
+
+ PRINT downloading the app info
+ curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/ca${component}-v3.zip &>>$LOG_FILE
+ cd /app
+
+ PRINT Extract the app content
+ unzip /tmp/${component}.zip &>>$LOG_FILE
+
+ cd /app
+
+ PRINT download NodeJS depedencies
+ npm install &>>$LOG_FILE
+
+ PRINT Start Service
+ systemctl daemon-reload &>>$LOG_FILE
+ systemctl enable ${component} &>>$LOG_FILE
+ systemctl restart ${component} &>>$LOG_FILE
+}
