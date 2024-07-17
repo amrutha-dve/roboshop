d40c0a296cfc01b6311828e11520289ab6a307cd
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-17 15:44:36 +0530

    redirecting

--- catalogue.sh
+++ catalogue.sh
@@ -2,8 +2,8 @@ source common.sh
 component=catalogue
 NODEJS
 
-echo install mongo client
-dnf install mongodb-mongosh -y >$LOG_FILE
+PRINT install mongo client
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 
-echo load master data
-mongosh --host mongo.dev.devopsb72.online </app/db/master-data.js >$LOG _FILE
+PRINT load master data
+mongosh --host mongo.dev.devopsb72.online </app/db/master-data.js &>>$LOG _FILE
