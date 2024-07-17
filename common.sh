018a80d172011240fc054c5d5c78fef74bc99d51
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-17 14:03:27 +0530

    redirecting

--- common.sh
+++ common.sh
@@ -8,40 +8,40 @@ rm -f $LOG_FILE
 #$}
 NODEJS() {
  echo Disable NodeJS Default Version
- dnf module disable nodejs -y >$LOG_FILE
+ dnf module disable nodejs -y &>$LOG_FILE
  echo enable NodeJS 20 Module
- dnf module enable nodejs:20 -y >$LOG_FILE
+ dnf module enable nodejs:20 -y &>$LOG_FILE
 
  echo Install NodeJS
- dnf install nodejs -y >$LOG_FILE
+ dnf install nodejs -y &>$LOG_FILE
 
  echo Copy Service File
- cp ${component}.service /etc/systemd/system/${component}.service >$LOG_FILE
+ cp ${component}.service /etc/systemd/system/${component}.service &>$LOG_FILE
 
  echo copy MongoDB repo File
- cp mongo.repo /etc/yum.repos.d/mongo.repo >$LOG_FILE
+ cp mongo.repo /etc/yum.repos.d/mongo.repo &>$LOG_FILE
 
  echo adding Application User
- useradd roboshop >$LOG_FILE
+ useradd roboshop &>$LOG_FILE
 
  echo Cleaning Old Content
- rm -rf /app >$LOG_FILE
+ rm -rf /app &>$LOG_FILE
 
  echo creating App Directory
- mkdir /app >$LOG_FILE
+ mkdir /app &>$LOG_FILE
 
 echo downloading the app info
- curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/ca${component}-v3.zip >$LOG_FILE
+ curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/ca${component}-v3.zip &>$LOG_FILE
  cd /app
 
 echo Extract the app content
- unzip /tmp/${component}.zip >$LOG_FILE
+ unzip /tmp/${component}.zip &>$LOG_FILE
 
 echo download NodeJS depedencies
- npm install >$LOG_FILE
+ npm install &>$LOG_FILE
 
  echo Start Service
- systemctl daemon-reload >$LOG_FILE
- systemctl enable ${component} >$LOG_FILE
- systemctl restart ${component} >$LOG_FILE
+ systemctl daemon-reload &>$LOG_FILE
+ systemctl enable ${component} &>$LOG_FILE
+ systemctl restart ${component} &>$LOG_FILE
 }
