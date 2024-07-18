f2fdf400a7aa231261d0b600576cd0c3a9efca67
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-19 03:38:32 +0530

    color update

--- common.sh
+++ common.sh
@@ -11,6 +11,8 @@ STAT() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
+    echo
+    echo "refer the log file for more info : log path : ${LOG_FILE}"
     exit $1
   fi
 }
