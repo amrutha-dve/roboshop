75d9515cee2db9c1c0f65da16d6d3ff261f1fd35
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-19 15:48:10 +0530

    update

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,18 @@
 source common.sh
-component=mongo
+component=mysql
 
-JAVA
+PRINT Start MySql service &>>$LOG_FILE
+STAT $?
+
+PRINT install mysql
+ dnf install mysql-server -y &>>$LOG_FILE
+ STAT $?
+
+ PRINT restart mysql
+ systemctl enable mysqld &>>$LOG_FILE
+ systemctl restart mysqld &>>$LOG_FILE
+ STAT $?
+
+ PRINT setuo  mysql root passwoqd
+ mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+ STAT $?
