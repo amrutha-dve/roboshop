e96fb0709e43132f8d97dcfa5f03f89849c837ed
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-07-19 06:01:30 +0530

    color update

--- redis.sh
+++ redis.sh
@@ -1,8 +1,20 @@
+PRINT disable redis
 dnf module disable redis -y
+STAT $?
+
+PRINT enable redis:7
 dnf module enable redis:7 -y
+STAT $?
+
+PRINT install redis
 dnf install redis -y
-## update redis file
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT updtae redis
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT restart redis service
 systemctl enable redis
 systemctl restart redis
+STAT $?
